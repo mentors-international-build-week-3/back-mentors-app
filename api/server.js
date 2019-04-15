ab283d1437ceeb9e0484ec66d28412e73d58d0e8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-15 15:29:24 -0400

    Creates a new variable 'client' (in server.js) that will allow for creating new messages to be sent to Twilio's API

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
+const client = new twilio(accountSid, authToken);
+
 server.use(express.json());
 
 
