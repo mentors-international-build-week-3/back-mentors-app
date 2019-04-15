7654b55c5298bb42926fefae949cbf30dc88fd7b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-15 17:39:41 -0400

    Creates a new 'MessageSchema' in the serverMongo.js file

--- api/serverMongo.js
+++ api/serverMongo.js
@@ -6,10 +6,20 @@ const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const mongoPassword = process.env.MONGO_USER_PASSWORD;
-
 const client = new twilio(accountSid, authToken);
 
 server.use(express.json());
+
+
+let MessageSchema = new mongoose.Schema({
+    phoneNumber: String,
+    groupName: String,
+    prepared: String,
+    apptDate: String
+});
+
+let Message = mongoose.model('Message', MessageSchema);
+
 
 mongoose
   .connect(
@@ -23,8 +33,15 @@ mongoose
     console.log(error);
   });
 
+
 server.get("/", (req, res) => {
   res.end();
+});
+
+server.post('/inbound', (req, res) => {
+    let from = req.body.From; // this refers to the mentor's phone number, which new messages will be sent FROM
+    let to = req.body.To; // this refers to the client's phone number, which is where the new messages will be sent TO
+    let body = req.body.Body; // this references the BODY of the new message that will be sent
 });
 
 module.exports = server;
