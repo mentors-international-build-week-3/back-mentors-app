f7ba5f76fa2256af0756b36754d1fc00804c874a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-15 16:01:21 -0400

    Creates an env variable for mongoDB password and connects to mongoDB using 'mongoose.connect()' statement in server.js

--- api/server.js
+++ api/server.js
@@ -5,10 +5,13 @@ const server = express();
 const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const mongoUserPassword = process.env.MONGO_USER_PASSWORD;
 
 const client = new twilio(accountSid, authToken);
 
 server.use(express.json());
+
+mongoose.connect(`mongodb+srv://ticotheps:${mongoUserPassword}>@mentors-db-hokei.mongodb.net/test?retryWrites=true`, {useMongoClient: true});
 
 
 
