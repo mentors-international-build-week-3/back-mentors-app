06856158c96ac30714be1017c3b063b741b63d41
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-15 15:26:17 -0400

    Creates env variables for Twilio credentials inside server.js

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const server = express();
 const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 server.use(express.json());
 
