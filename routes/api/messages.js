a34066348c4718b7b493ecc6765608c5b1587f57
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 12:36:47 -0400

    Creates a route for GET requests and a route for POST requests to the '/api/messages' endpoint

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -4,19 +4,43 @@ const router = express.Router();
 // Imports the Message Model (so we can query the MongoDB)
 const Message = require('../../models/Message');
 
+
 // @route   GET request to the '/api/messages' endpoint
 // @desc    Retrieves all messages from MongoDB
 // @access  Public 
 router.get('/', (req, res) => {
-    Item.find()
-        .sort({ date: -1 }) // sorts all retrieved items by date; "-1" = descending order, "1" = ascending order 
-        .then(items => {
-            res.status(200).json(items)
+    Message.find()
+        .sort({ date: -1 }) // sorts all retrieved messages by date; "-1" = descending order, "1" = ascending order 
+        .then(messages => {
+            res.status(200).json(messages);
         })
         .catch(err => {
             res.status(500).res.json(err);
         });
 });
+
+
+// @route   POST request to '/api/messages'
+// @desc    Creates a new message
+// @access  Public 
+router.post('/', (req, res) => {
+    // 'new' refers to us creating a new instance of the 'Item' model we made in Item.js
+    // 'Item' refers to the 'Item' model (template) we created in Item.js
+    const newMessage = new Message({
+        clientName: req.body.clientName
+    }); 
+
+     
+    newMessage
+        .save() // saves the new message to our MongoDB
+        .then(message => {
+            res.json(message);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
 
 
 
