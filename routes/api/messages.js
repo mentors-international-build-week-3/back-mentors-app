fd1692b857e66ad7b0b49b6853aa53595db0cdd9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-16 18:17:41 -0400

    At this point, messages can be received from mobile devices via ngrok's URL, which forwards those messages to our local host using a webhook

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -25,26 +25,111 @@ router.get('/', (req, res) => {
 // @desc    Creates a new message document
 // @access  Public 
 router.post('/', (req, res) => {
+
+    let from = req.body.From; // refers to mentor's phone number
+    let to = req.body.To; // refers to the mentee's phone number
+    let body = req.body.Body; // refers to the BODY of the new message that is sent
+  
+    Message.find({ phoneNumber: req.body.From }, (err, message) => {
+        console.log("The message was received with the ngrok URL and was forwarded to our localhost with a webhook!", body);
+    
+        // if (message.length !== 0) {
+        //   // this means there is a conversation stored in our mongoDB that we can continue
+        //   if (
+        //     !message[0].studentName &&
+        //     !message[0].studentClass &&
+        //     !message[0].apptDate
+        //   ) {
+        //     // if there is no matching 'studentName', 'studentClass', or 'apptDate' info in our DB,
+        //     // then we know this is the info the clients wants to update
+        //     Message.findByIdAndUpdate(
+        //       message[0]._id,
+        //       { $set: { studentName: body } },
+        //       { new: true, upsert: true },
+        //       () => {
+        //         client.messages.create({
+        //           to: `${from}`,
+        //           from: `${to}`,
+        //           body: "Which mentorship training class do you attend?"
+        //         });
+    
+        //         res.end();
+        //       }
+        //     );
+        //   } else if (!message[0].studentClass && !message[0].apptDate) {
+        //     // if there is no matching 'studentClass' or 'apptDate' info in our DB, then we know this is the info the clients wants to update
+        //     Message.findByIdAndUpdate(
+        //       message[0]._id,
+        //       { $set: { studentClass: body } },
+        //       { new: true, upsert: true },
+        //       () => {
+        //         client.messages.create({
+        //           to: `${from}`,
+        //           from: `${to}`,
+        //           body: "What is the date of your next mentorship session?"
+        //         });
+    
+        //         res.end();
+        //       }
+        //     );
+        //   } else if (!message[0].apptDate) {
+        //     // if there is no matching 'apptDate' info in our DB, then we know this is the info the clients wants to update
+        //     Message.findByIdAndUpdate(
+        //       message[0]._id,
+        //       { $set: { apptDate: body } },
+        //       { new: true, upsert: true },
+        //       () => {
+        //         client.messages.create({
+        //           to: `${from}`,
+        //           from: `${to}`,
+        //           body:
+        //             "Thanks for setting up your automated mentors int'l text reminder! See you soon!"
+        //         });
+    
+        //         res.end();
+        //       }
+        //     );
+        //   }
+        // } else {
+        //   if (body === "RSVP") {
+        //     let newMessage = new Message();
+        //     newMessage.phoneNumber = from;
+        //     newMessage.save(() => {
+        //       client.messages.create({
+        //         // this creates a new return message to send BACK to the owner of the incoming text
+        //         to: `${from}`, // the 'from' refers to the sender's phone number, which now becomes the receiver's phone number for our new return message
+        //         from: `${to}`, // the 'to' refers to the Mentors App phone number, which now becomes the sender's phone number in the new return message
+        //         body: "Hi! What's your name?"
+        //       });
+    
+        //       res.end();
+        //     });
+        //   }
+        // }
+    
+        res.end();
+    });
+
     // 'new' refers to the creation of a new instance of the 'Message' model from Message.js
     // 'Message' refers to the use of the 'Message' model (from Schema) as a template to build our new instance
-    const newMessage = new Message({
-        menteeFirstName: req.body.menteeFirstName,
-        menteeLastName: req.body.menteeLastName,
-        phoneNumber: req.body.phoneNumber,
-        attending: req.body.attending,
-        noAttendReason: req.body.noAttendReason,
-        needBooks: req.body.needBooks
-    }); 
+    // const newMessage = new Message({
+    //     menteeFirstName: req.body.menteeFirstName,
+    //     menteeLastName: req.body.menteeLastName,
+    //     phoneNumber: req.body.phoneNumber,
+    //     attending: req.body.attending,
+    //     noAttendReason: req.body.noAttendReason,
+    //     needBooks: req.body.needBooks
+    // }); 
 
      
-    newMessage
-        .save() // saves the new message object as a new document in the MongoDB
-        .then(message => {
-            res.json(message);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+    // newMessage
+    //     .save() // saves the new message object as a new document in the MongoDB
+    //     .then(message => {
+    //         res.json(message);
+    //     })
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
 });
 
 
@@ -103,90 +188,3 @@ router.put('/:id', (req, res) => {
 
 
 module.exports = router;
-
-  
-//   server.post("/inbound", (req, res) => {
-//     let from = req.body.From; // this refers to the mentor's phone number, which new messages will be sent FROM
-//     let to = req.body.To; // this refers to the client's phone number, which is where the new messages will be sent TO
-//     let body = req.body.Body; // this references the BODY of the new message that will be sent
-  
-//     Message.find({ phoneNumber: req.body.From }, (err, message) => {
-//       // console.log(message);
-  
-//       if (message.length !== 0) {
-//         // this means there is a conversation stored in our mongoDB that we can continue
-//         if (
-//           !message[0].studentName &&
-//           !message[0].studentClass &&
-//           !message[0].apptDate
-//         ) {
-//           // if there is no matching 'studentName', 'studentClass', or 'apptDate' info in our DB,
-//           // then we know this is the info the clients wants to update
-//           Message.findByIdAndUpdate(
-//             message[0]._id,
-//             { $set: { studentName: body } },
-//             { new: true, upsert: true },
-//             () => {
-//               client.messages.create({
-//                 to: `${from}`,
-//                 from: `${to}`,
-//                 body: "Which mentorship training class do you attend?"
-//               });
-  
-//               res.end();
-//             }
-//           );
-//         } else if (!message[0].studentClass && !message[0].apptDate) {
-//           // if there is no matching 'studentClass' or 'apptDate' info in our DB, then we know this is the info the clients wants to update
-//           Message.findByIdAndUpdate(
-//             message[0]._id,
-//             { $set: { studentClass: body } },
-//             { new: true, upsert: true },
-//             () => {
-//               client.messages.create({
-//                 to: `${from}`,
-//                 from: `${to}`,
-//                 body: "What is the date of your next mentorship session?"
-//               });
-  
-//               res.end();
-//             }
-//           );
-//         } else if (!message[0].apptDate) {
-//           // if there is no matching 'apptDate' info in our DB, then we know this is the info the clients wants to update
-//           Message.findByIdAndUpdate(
-//             message[0]._id,
-//             { $set: { apptDate: body } },
-//             { new: true, upsert: true },
-//             () => {
-//               client.messages.create({
-//                 to: `${from}`,
-//                 from: `${to}`,
-//                 body:
-//                   "Thanks for setting up your automated mentors int'l text reminder! See you soon!"
-//               });
-  
-//               res.end();
-//             }
-//           );
-//         }
-//       } else {
-//         if (body === "RSVP") {
-//           let newMessage = new Message();
-//           newMessage.phoneNumber = from;
-//           newMessage.save(() => {
-//             client.messages.create({
-//               // this creates a new return message to send BACK to the owner of the incoming text
-//               to: `${from}`, // the 'from' refers to the sender's phone number, which now becomes the receiver's phone number for our new return message
-//               from: `${to}`, // the 'to' refers to the Mentors App phone number, which now becomes the sender's phone number in the new return message
-//               body: "Hi! What's your name?"
-//             });
-  
-//             res.end();
-//           });
-//         }
-//       }
-  
-//       res.end();
-//     });
-//   });
