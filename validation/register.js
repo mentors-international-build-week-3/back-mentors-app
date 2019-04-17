d054c90ecca0f6a568fc67263a9e571acd0090d6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-17 14:52:12 -0400

    Creates a register.js file to validate user's information during signup process

--- validation/register.js
+++ validation/register.js
@@ -11,15 +11,59 @@ module.exports = function validateRegisterInput(data) {
     data.password = !isEmpty(data.password) ? data.password : "";
     data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-    // checks name field
-    if (Validator.isEmpty(data.name)) {
-        errors.name = "Name field is required";
+    // checks username field
+    if (Validator.isEmpty(data.username)) {
+        errors.username = "Username field is required";
     }
+
+    // checks password field
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }   
+
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = "Confirm password field is required";
+    }
+
+    if (!Validator.isLength(data.password, { min: 10, max: 50 })) {
+        errors.password = "Password must be at least 10 characters";
+    }
+
+    if (Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match";
+    }
+
+    // checks userFirstName field
+    if (Validator.isEmpty(data.userFirstName)) {
+        errors.userFirstName = "First name field is required";
+    }    
+
+    // checks userLastName field
+    if (Validator.isEmpty(data.userLastName)) {
+        errors.userLastName = "Last name field is required";
+    }  
+    
+    // checks userType field
+    if (Validator.isEmpty(data.userType)) {
+        errors.userType = "User type field is required";
+    }    
+
+    // checks phoneNumber field
+    if (Validator.isEmpty(data.phoneNumber)) {
+        errors.phoneNumber = "Phone number field is required";
+    } else if (!Validator.isMobilePhone(data.phoneNumber)) {
+        errors.phoneNumber = "Phone number is invalid";
+    }   
 
     // checks email field
     if (Validator.isEmpty(data.email)) {
         errors.email = "Email field is required";
     } else if (!Validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
-    }
-}
+    }    
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
