443167ae36846d2b70ec80d3226d9f59056a47fe
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-17 17:21:33 -0400

    Changes login.js to only require email and password upon login for users

--- validation/login.js
+++ validation/login.js
@@ -7,11 +7,7 @@ module.exports = function validateLoginInput(data) {
 
     // converts empty fields to an empty string so we can run validator
     data.email = !isEmpty(data.email) ? data.email : "";
-    data.username = !isEmpty(data.username) ? data.username : "";
     data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-    data.phoneNumber = !isEmpty(data.phoneNumber) ? data.phoneNumber : "";
-    
 
     // checks email field
     if (Validator.isEmpty(data.email)) {
@@ -23,19 +19,7 @@ module.exports = function validateLoginInput(data) {
     // checks password field
     if (Validator.isEmpty(data.password)) {
         errors.password = "Password field is required";
-    }   
-
-    if (Validator.isEmpty(data.password2)) {
-        errors.password2 = "Confirm password field is required";
     }
-
-    if (!Validator.isLength(data.password, { min: 8, max: 50 })) {
-        errors.password = "Password must be at least 8 characters";
-    }
-
-    if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = "Passwords must match";
-    } 
 
     return {
         errors,
