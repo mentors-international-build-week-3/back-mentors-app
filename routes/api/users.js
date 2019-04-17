2f30221863dff342b7ee4383d3f49f7477edb33d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-17 17:13:10 -0400

    Reformats users.js file with prettier

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,184 +1,193 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 
-
 // imports input validation
-const validateSignupInput = require('../../validation/signup');
-const validateLoginInput = require('../../validation/login');
-
+const validateSignupInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
 
 // imports the User Model (so we can query the 'users' collection in the MongoDB)
-const User = require('../../models/User');
-
+const User = require("../../models/User");
 
 // @route   GET request to the '/api/users' endpoint
 // @desc    Retrieves all users from MongoDB
-// @access  Public 
-router.get('/', (req, res) => {
-    User
-        .find()
-        .sort({ createdDate: -1 }) // sorts all retrieved users by createdDate; "-1" = descending order, "1" = ascending order 
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+// @access  Public
+router.get("/", (req, res) => {
+  User.find()
+    .sort({ createdDate: -1 }) // sorts all retrieved users by createdDate; "-1" = descending order, "1" = ascending order
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
-
 
 // @route   POST request to the '/api/users/signup' endpoint
 // @desc    Creates a new user document
-// @access  Public 
-router.post('/signup', (req, res) => {
-    // validates form input
-    const { errors, isValid } = validateSignupInput(req.body);
+// @access  Public
+router.post("/signup", (req, res) => {
+  // validates form input
+  const { errors, isValid } = validateSignupInput(req.body);
 
-    // checks validation
-    if (!isValid) {
-        return res.status(400).json(errors);
+  // checks validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
     }
 
-    User
-        .findOne({ email: req.body.email })
-        .then(user => {
-        if (user) {
-            return res.status(400).json({ email: "Email already exists" });
-        }
+    // 'new' refers to the creation of a new instance of the 'User' model from User.js
+    // 'User' refers to the use of the 'User' model (from Schema) as a template to build our new instance
+    const newUser = new User({
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      userFirstName: req.body.userFirstName,
+      userLastName: req.body.userLastName,
+      userType: req.body.userType,
+      phoneNumber: req.body.phoneNumber
+    });
 
-        // 'new' refers to the creation of a new instance of the 'User' model from User.js
-        // 'User' refers to the use of the 'User' model (from Schema) as a template to build our new instance
-        const newUser = new User({
-			email: req.body.email,
-            username: req.body.username,
-            password: req.body.password,
-            userFirstName: req.body.userFirstName,
-            userLastName: req.body.userLastName,
-            userType: req.body.userType,
-            phoneNumber: req.body.phoneNumber
-        });
-
-        // hashes password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser
-                    .save()
-                    .then(user => res.json(user))
-                    .catch(err => console.log(err));
-            });
-        });   
-    }); 
+    // hashes password before saving in database
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
+      });
+    });
+  });
 });
-
 
 // @route 	POST requst to the 'api/users/login' endpoint
 // @desc 	Logs user in and returns JWT token
 // @access 	Public
-	router.post('/login', (req, res) => {
-		// Form validation
-		const { errors, isValid } = validateLoginInput(req.body);
-	
-		// Check validation
-		if (!isValid) {
-			return res.status(400).json(errors);
-		}
-		
-		const email = req.body.email;
-		const password = req.body.password;
+router.post("/login", (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
 
-		// Find user by email
-		User
-			.findOne({ email })
-			.then(user => {
-				// Checks if user exists
-				if (!user) {
-					return res.status(404).json({ emailnotfound: "Email not found" });
-				}
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-				// Checks user's input hash against hash in the MongoDB
-				bcrypt
-					.compare(password, user.password)
-					.then(isMatch => {
-						if (isMatch) {	
-							// User matches
-							// Creates a JWT Payload
-							const payload = {
-								id: user.id,
-								name: user.name
-							};
+  const email = req.body.email;
+  const password = req.body.password;
 
-							// Signs the token
-							jwt.sign(payload, keys.secretOrKey, {expiresIn: 21600}, (err, token) => { // 21,600 sec => 6 hours
-								res.json({ success: true, token: "Bearer " + token });
-							});
-						} else {
-							return res.status(400).json({ passwordincorrect: "Password incorrect" });
-						}
-					});
-			});
-	});
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Checks if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
 
+    // Checks user's input hash against hash in the MongoDB
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matches
+        // Creates a JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        // Signs the token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 21600 },
+          (err, token) => {
+            // 21,600 sec => 6 hours
+            res.json({ success: true, token: "Bearer " + token });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
 
 // @route   DELETE request to the 'api/users/:id' endpoint
 // @desc    Deletes a specific user
-// @access  Public 
-router.delete('/:id', (req, res) => {
-    User
-        .findById(req.params.id)
-        .then(user => {
-            user
-                .remove()
-                .then(() => {
-                    res.status(202).json({ delete_success: true });
-                })
-                .catch(err => {
-                    res.status(404).json({ error_message: "Something went wrong while trying to delete this user from the database" });
-                });
+// @access  Public
+router.delete("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user
+        .remove()
+        .then(() => {
+          res.status(202).json({ delete_success: true });
         })
         .catch(err => {
-            res.status(500).json({ error_message: "User was not deleted because this user could not be found in the database" });
+          res
+            .status(404)
+            .json({
+              error_message:
+                "Something went wrong while trying to delete this user from the database"
+            });
         });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error_message:
+            "User was not deleted because this user could not be found in the database"
+        });
+    });
 });
-
 
 // @route   PUT request to 'api/users/:id'
 // @desc    Updates a specific user's document
-// @access  Public 
-router.put('/:id', (req, res) => {
-    User
-        .findById(req.params.id)
-        .then((user, err) => {
-            if (!user) {
-
-                res.status(404).json({ message: "User was not updated because this user could not be found" });
-            } else {
-
-				(req.body.email) ? user.email = req.body.email : null;
-                (req.body.username) ? user.username = req.body.username : null;
-                (req.body.password) ? user.password = req.body.password : null;
-                (req.body.userFirstName) ? user.userFirstName = req.body.userFirstName : null;
-                (req.body.userLastName) ? user.userLastName = req.body.userLastName : null;
-                (req.body.userType) ? user.userType = req.body.userType : null;
-                (req.body.phoneNumber) ? user.phoneNumber = req.body.phoneNumber : null;
-                
-                user
-                    .save()
-                    .then(user => {
-                        res.status(202).json({ message: "User was updated successfully" });
-                    })
-                    .catch(err => {
-                        res.status(400).json({ message: "Something went wrong while trying to update this user" });
-                    });
-            }
+// @access  Public
+router.put("/:id", (req, res) => {
+  User.findById(req.params.id).then((user, err) => {
+    if (!user) {
+      res
+        .status(404)
+        .json({
+          message: "User was not updated because this user could not be found"
         });
+    } else {
+      req.body.email ? (user.email = req.body.email) : null;
+      req.body.username ? (user.username = req.body.username) : null;
+      req.body.password ? (user.password = req.body.password) : null;
+      req.body.userFirstName
+        ? (user.userFirstName = req.body.userFirstName)
+        : null;
+      req.body.userLastName
+        ? (user.userLastName = req.body.userLastName)
+        : null;
+      req.body.userType ? (user.userType = req.body.userType) : null;
+      req.body.phoneNumber ? (user.phoneNumber = req.body.phoneNumber) : null;
+
+      user
+        .save()
+        .then(user => {
+          res.status(202).json({ message: "User was updated successfully" });
+        })
+        .catch(err => {
+          res
+            .status(400)
+            .json({
+              message: "Something went wrong while trying to update this user"
+            });
+        });
+    }
+  });
 });
-
-
 
 module.exports = router;
