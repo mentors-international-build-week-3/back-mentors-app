11fe77196844881d507e7c3b55ad0b15b128ecdc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-17 19:23:37 -0400

    Adds @material-ui/icons as a dependency inside 'client', imports Roboto font from Material-UI via CDN link in <head> of index.html, imports <Navbar /> and <Landing /> into App.js, and wraps <App /> with <Router> and </Router>

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>What's good, homie?</h1>
+        <Navbar />
+        <Landing />
       </div>
     );
   }
