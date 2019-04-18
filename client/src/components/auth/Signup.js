ba4136e8adafdeaa40b976b93ee23deeea753243
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-18 13:10:58 -0400

    Imports withRouter into Signup.js

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { signupUser } from "../../actions/authActions";
