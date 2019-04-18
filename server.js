0282293c6f55679723cc4fb37d06dae940f5998d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-18 16:05:13 -0400

    Modifies the 'if' statement used to set a static folder of production files (from client) for deployment to Heroku

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ app.use('/api/users', users);
 // serves static assets if in production
 if(process.env.NODE_ENV === 'production') {
     // sets static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "client", "build")));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
