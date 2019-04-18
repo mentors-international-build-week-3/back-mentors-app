f3fd916caf4b1e0186c140f640f5e42017392924
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-18 10:53:19 -0400

    Imports 'Provider' and 'store' (for Redux) into App.js; also creates a 'store' folder in the root directory of 'client'

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import { Provider } from "react-redux";
+import store from './store';
+
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -12,20 +15,23 @@ import Login from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route 
-          exact 
-          path="/signup" 
-          render={props => <Signup {...props} />}
-        />
-        <Route 
-          exact 
-          path="/login" 
-          render={props => <Login {...props} />}
-        />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route 
+            exact 
+            path="/signup" 
+            render={props => <Signup {...props} />}
+          />
+          <Route 
+            exact 
+            path="/login" 
+            render={props => <Login {...props} />}
+          />
+        </div>
+      </Provider>
+
     );
   }
 }
