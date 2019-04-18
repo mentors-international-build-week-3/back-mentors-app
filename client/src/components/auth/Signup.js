6d2e262e3f2d6b69bba336405f0805a94972cccb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-18 13:42:16 -0400

    Fixes type in onSubmit() function in Signup.js

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -40,7 +40,7 @@ class Signup extends Component {
         password2: this.state.password2
         };
 
-        this.props.registerUser(newUser, this.props.history);
+        this.props.signupUser(newUser, this.props.history);
     };
 
 
