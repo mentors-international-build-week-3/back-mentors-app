2cea074129e1d4f3c29df9d4ec2bdd8332cea9cd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-18 10:33:48 -0400

    Creates a <Login /> and a <Signup />

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+
 import './App.css';
-
-
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
 
 
 class App extends Component {
@@ -11,7 +14,17 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route 
+          exact 
+          path="/signup" 
+          render={props => <Signup {...props} />}
+        />
+        <Route 
+          exact 
+          path="/login" 
+          render={props => <Login {...props} />}
+        />
       </div>
     );
   }
